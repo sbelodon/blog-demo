a5c29be0c699280d997ec21eab68a027eafb133c
sbelodon <belodon.sergey@gmail.com>
2019-09-12 19:06:45 +0300

    added liquibase update command to redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 yarn install --network-timeout 1000000
 mvn clean install -P dev-postgres
+java -jar backend/target/dependency/liquibase.jar --defaultsFile=backend/target/classes/liquibase.properties update
 java -jar backend/target/blog-demo-app.jar
