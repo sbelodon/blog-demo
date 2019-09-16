a4919b030935ad58880ac8e07e45596664c17ec8
sbelodon <belodon.sergey@gmail.com>
2019-09-16 12:23:41 +0300

    added remote debug params

--- redeploy.sh
+++ redeploy.sh
@@ -2,4 +2,4 @@
 yarn install --network-timeout 1000000
 mvn clean install -P dev-postgres
 java -jar backend/target/dependency/liquibase.jar --defaultsFile=backend/target/classes/liquibase.properties update
-java -jar backend/target/blog-demo-app.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar backend/target/blog-demo-app.jar
